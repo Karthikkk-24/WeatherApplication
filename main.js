8301f3e2884f1914c40694bf599ab53acfc73cd6
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-03-07 11:24:34 +0530

    Completed

---
+++ main.js
@@ -0,0 +1,56 @@
+const apiKey = "0153d4ab127d8086b11ca42f21bb5b3a";
+
+const locationInput = document.querySelector(".location input");
+const resultsContainer = document.querySelector(".weather_card .results");
+const noResultsContainer = document.querySelector(".weather_card .no_results");
+
+const fetchWeatherData = async (location) => {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+    );
+    const data = await response.json();
+    return data;
+};
+
+
+
+
+const renderWeatherData = (data) => {
+    const { name, main, weather } = data;
+    let weatherCondition;
+    if (weather[0].main == 'Clouds') {
+        weatherCondition = 'images/clouds.png';
+    } else if (weather[0].main == 'Clear') {
+        weatherCondition = 'images/sun.png';
+    } else {
+        weatherCondition = 'images/sun.png';
+    }
+    const weatherCard = `
+    <div class="weather_card_container">
+      <h2>${name}</h2>
+      <img class="weather_img" src="${weatherCondition}" />
+      <p>Temperature: ${main.temp}°C</p>
+      <p>Feels like: ${main.feels_like}°C</p>
+      <p>Humidity: ${main.humidity}%</p>
+    </div>
+  `;
+    resultsContainer.innerHTML = weatherCard;
+    noResultsContainer.style.display = "none";
+};
+
+const handleWeatherDataFetchError = () => {
+    noResultsContainer.style.display = "flex";
+    resultsContainer.innerHTML = "";
+};
+
+locationInput.addEventListener("keyup", async (event) => {
+    if (event.keyCode === 13) {
+        const location = event.target.value;
+        try {
+            const data = await fetchWeatherData(location);
+            renderWeatherData(data);
+        } catch (error) {
+            handleWeatherDataFetchError();
+        }
+    }
+});
